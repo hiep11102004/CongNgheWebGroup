58269dec16442ac9d514070f89871c1278bb113b
hiep11102004 <hiep1110nd@gmail.com>
2024-12-07 19:22:24 +0700

    Thong tin

---
+++ views/news/detail.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chi tiết tin tức</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="my-4"><?php echo $news['title']; ?></h1>
+        <img src="uploads/<?php echo $news['image']; ?>" class="img-fluid" alt="image">
+        <p class="mt-4"><?php echo $news['content']; ?></p>
+        <a href="index.php" class="btn btn-secondary">Quay lại</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
