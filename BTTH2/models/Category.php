0d8159d27bef6fe065969e13621cf5f3062aefe5
Đào Văn Nghĩa <147342871+canhdongyeuthuong@users.noreply.github.com>
2024-12-07 20:18:12 +0700

    Delete BTTH2 directory

--- BTTH2/models/Category.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Category {
-    public static function getAll() {
-        $db = Database::connect();
-        $stmt = $db->query("SELECT * FROM categories");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-}
-?>
